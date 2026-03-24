b70b03eeb454b7e90075f467c41ef2af7871750a
ekresanovic <ekresanovic@gmail.com>
2026-03-24 14:17:19 +0100

    Ajout de app.user.id dans nav, affichage de page de profil readonly, écriture d'une fonction requete avec jointure dans UserRepository.php mais commenté car nous n'avons pas encore besoin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/user', name: 'user_')]
 final class UserController extends AbstractController
 {
-    #[Route('/profile', name: 'profile', methods: ['GET'])]
-    public function getUserProfile(UserRepository $userRepository): Response
+    #[Route('/profile/{id}', name: 'profile', methods: ['GET'])]
+    public function getUserProfile(User $id): Response
     {
-        $user = $this->getUser();
         return $this->render('user/profile.html.twig', [
-            'user' => $user,
+            'user' => $id,
         ]);
     }
 }
