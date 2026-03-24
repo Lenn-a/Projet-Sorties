6a0aa502bbf6794e947168a68b6984d6c07a44a9
aquintard2025 <adrien.quintard@gmail.com>
2026-03-24 11:59:49 +0100

    formulaire create

--- src/Form/OutingType.php
+++ src/Form/OutingType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -9,10 +10,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class OutingType
+class OutingType extends AbstractType
 {
 
-    public function bildForm(FormBuilderInterface $builder, array $options): void{
+    public function buildForm(FormBuilderInterface $builder, array $options): void{
         $builder
             ->add('name', TextType::class, [
                 'label'=>'Your outing name',
@@ -35,14 +36,25 @@ class OutingType
             ->add('signUpDateLimit', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('nbSignUpMax', TextType::class, [
+            ->add('nbSignupsMax', TextType::class, [
                 'label'=>'Number of sign up max',
             ])
-            ->add('outingInfos', TextareaType::class, [
+            ->add('outingInfo', TextareaType::class, [
                 'label'=>'Your outing infos',
             ])
             ->add('photo', FileType::class, [
                 'mapped' => false,
+            ])
+            ->add('campus', ChoiceType::class, [
+                'choices' => [
+                    'Nantes'=>'Nantes',
+                    'Rennes'=>'Rennes',
+                    'Niort'=>'Niort',
+                    'Quimpert'=>'Quimpert',
+                ]
+            ])
+            ->add('location', TextType::class, [
+                'label'=>'Your location',
             ]);
     }
 }
