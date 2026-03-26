6dc640afd6fa224d691c238fb1ab5a5b9d14ff30
aquintard2025 <adrien.quintard@gmail.com>
2026-03-26 11:11:44 +0100

    OutingController -modif fonction create() -ajout route "privateList" Template : privateList.html.twig Nav : ajout bouton "mes sorties" OutingRepository : fonction findMyOuting()

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -6,6 +6,7 @@ use App\Entity\Outing;
 use App\Form\OutingType;
 use App\Repository\OutingRepository;
 use App\Repository\StatusRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -57,6 +58,7 @@ final class OutingController extends AbstractController
     public function create(
         EntityManagerInterface $entityManager,
         StatusRepository $statusRepository,
+        UserRepository $userRepository,
         Request $request,
     ): Response {
         $outing = new Outing();
@@ -64,83 +66,58 @@ final class OutingController extends AbstractController
         $outingForm = $this->createForm(OutingType::class, $outing);
         $outingForm->handleRequest($request);
 
-        if ($outingForm->isSubmitted() && $outingForm->isValid()) {
-            //User making the Outing is its organiser :
-            $outing->setOrganiser($this->getUser());
+        $action = $request->request->get('action');
 
-            //Organiser is a participant by default :
-            $outing->addParticipant($this->getUser());
+        if ($action === 'publier'){
+            if($outingForm->isSubmitted() && $outingForm->isValid()){
+                //Le user qui créer la sortie = organisateur
+                $outing->setOrganiser($this->getUser());
+                //Organisateur est participant par défault
+                $outing->addParticipant($this->getUser());
+                //Status = ouvert quand on clic sur "Publier"
+                $published = $statusRepository->getStatusByName('Ouvert');
+                $outing->setStatus($published);
 
-            //Assign status 'Ouverte' when outing is published
-            $published = $statusRepository->getStatusByName('Ouverte');
-            $outing->setStatus($published);
+                $entityManager->persist($outing);
+                $entityManager->flush();
 
-            $entityManager->persist($outing);
-            $entityManager->flush();
+                $this->addFlash('success', 'La sortie ' .$outing->getName(). ' a bien été publiée.');
+                //redirection vers la page de détail de la sortie
+                return $this->redirectToRoute('outing_details', ['id' => $outing->getId()]);
+            }
+        }
 
-            $this->addFlash('success', 'Outing' . $outing->getName() . ' has been added.');
+        if($action === 'enregistrer'){
+            if($outingForm->isSubmitted() && $outingForm->isValid()){
+                $outing->setOrganiser($this->getUser());
+                $outing->addParticipant($this->getUser());
+                //Status = en création si on clic sur "enregistrer"
+                $published = $statusRepository->getStatusByName('En création');
+                $outing->setStatus($published);
 
-            return $this->redirectToRoute('outing_details', ['id' => $outing->getId()]);
+                $entityManager->persist($outing);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'La sortie ' .$outing->getName(). ' a bien été enregistrée.');
+
+                //Redirection vers la liste de sorties enregistrées
+                return $this->redirectToRoute('outing_privateList', ['id' => $this->getUser()->getId()]);
+            }
         }
         return $this->render('outing/create.html.twig', [
-            'outingForm' => $outingForm
+            'outingForm'=> $outingForm
         ]);
 }
 
-    //retourne le formulaire pour update une sortie
-//    #[Route('/update/{id}', name: 'update', requirements: ['id' => '\d+'])]
-//    public function update(
-//        int $id,
-//        OutingRepository $outingRepository,
-//        Request $request,
-//        EntityManagerInterface $entityManager,
-//    ){
-//        $outing = $outingRepository->find($id);
-//        $outingForm = $this->createForm(OutingType::class, $outing);
-//        $outingForm->handleRequest($request);
-//        if ($outingForm->isSubmitted() && $outingForm->isValid()) {
-//            $entityManager->persist($outing);
-//            $entityManager->flush();
-//            $this->addFlash('success', 'Votre sortie' . $outing->getName() . ' a été mise à jour.');
-//            return $this->redirectToRoute('outing_details', ['id' => $outing->getId()]);
-//        }
-//        return $this->render('outing/update.html.twig', [
-//            'outing' => $outing,
-//        ]);
-//    }
+    //Liste privé d'un utilisateur
+    #[Route('/privateList/{id}', name: 'privateList')]
+    public function privateList(OutingRepository $outingRepository): Response{
+        $outings = $outingRepository->findMyOutings();
 
-
-
-
-
-//il faut aussi une route pour acceder à une "privateList"
-// qui affiche les sorties dont l'utilisateur ets l'organisateur.
-
-//    #[Route('/save', name: 'save', methods: ['GET', 'POST'])]
-//    public function save(
-//        EntityManagerInterface $entityManager,
-//        Request $request,
-//        OutingRepository $outingRepository,
-//        int $id,
-//    ): Response {
-//        $outing = new Outing();
-//        $outingForm = $this->createForm(OutingType::class, $outing);
-//        $outingForm->handleRequest($request);
-//
-//        if ($outingForm->isSubmitted() && $outingForm->isValid()) {
-//
-//
-//            $entityManager->persist($outing);
-//            $entityManager->flush();
-//
-//            $this->addFlash('success', 'La sortie ' . $outing->getName() . ' a été sauvgardée.');
-//
-//            return $this->redirectToRoute('outing_save', ['id' => $outing->getId()]);
-//        }
-//        return $this->render('outing/privateList.html.twig', [
-//            'outingForm' => $outingForm
-//        ]);
-//    }
+        return $this->render('outing/privateList.html.twig', [
+            'outings' => $outings
+        ]);
+    }
 
     #[Route('/participate/{id}', name: 'participate', requirements: ['id' => '\d+'])]
     public function participateInOuting(int $id,
